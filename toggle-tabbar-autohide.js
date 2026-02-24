57b9b67c2b4b3817945ef5881f9dc92a7ce5740b
𝕲𝖍𝖔𝖘𝖙𝖞 | 🕷 <me7war@proton.me>
2026-02-25 00:39:41 +0200

    Update toggle-tabbar-autohide.js

--- toggle-tabbar-autohide.js
+++ toggle-tabbar-autohide.js
@@ -3,18 +3,16 @@
 // @version     1.0.0
 // @description Toggle the "autohide-tabs" class on #browser
 // ==/UserScript==
-
-(function(){
+// Toggle Auto-Hide Tabbar
+(function() {
   const B = document.getElementById("browser");
   if (!B) return;
-
   const CLASS = "autohide-tabs";
-
   if (B.classList.contains(CLASS)) {
     B.classList.remove(CLASS);
-    console.log("Tabbar auto-hide: OFF");
+    console.log("Auto-hide: Disabled");
   } else {
     B.classList.add(CLASS);
-    console.log("Tabbar auto-hide: ON");
+    console.log("Auto-hide: Enabled");
   }
 })();
